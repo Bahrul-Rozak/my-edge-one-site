89b6810148eb396e4ea47a1d6a45a754ede3fe44
bahrulrozak <bahrulrozak02@gmail.com>
2026-01-20 10:10:58 +0700

    useless code

--- edge-functions/api/greet.js
+++ edge-functions/api/greet.js
@@ -1,30 +1,28 @@
 export async function onRequestPost(context) {
-    const {name} = await context.request.json()
+  const { name } = await context.request.json();
 
-    const data = {
-        message: `Halo ${name || 'Pengunjung'}! 
-        Salam dari Edge Function di ${context.request.cf?.city || 'lokasi yang dekat denganmu'}!`,
-        timestamp: new Date().toISOString(),
-        clientIP : context.request.headers.get('cf-connecting-ip')
+  const data = {
+    message: `Halo, ${name || 'Pengunjung'}! Salam dari Edge Function di ${context.request.cf?.city || 'lokasi yang dekat denganmu'}!`,
+    timestamp: new Date().toISOString(),
+    clientIP: context.request.headers.get('cf-connecting-ip') 
+  };
+
+  
+  return new Response(JSON.stringify(data, null, 2), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Access-Control-Allow-Origin': '*'
     }
-
-    return new Response(JSON.stringify(data, null, 2), {
-        status: 200,
-        headers: {
-            'Content-Type' : 'application/json; charset=UTF-8',
-            'Access-Control-Allow-Origin' : '*'
-        }
-    })
-    
+  });
 }
 
-export async function onRequestGet(context){
-    const data = {
-        instruction : 'Kirim POST request dengan JSON body {"name" : "Nama Anda"}',
-        endpoint: 'api/greet'
-    }
-
-    return new Response(JSON.stringify(data, null, 2), {
-        headers: {'Content-Type': 'application/json'}
-    })
+export async function onRequestGet(context) {
+  const data = {
+    instruction: 'Kirim POST request dengan JSON body { "name": "Nama Anda" }',
+    endpoint: '/api/greet'
+  };
+  return new Response(JSON.stringify(data, null, 2), {
+    headers: { 'Content-Type': 'application/json' }
+  });
 }
